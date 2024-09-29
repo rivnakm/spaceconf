61907b12dcd654bf131263c999f0f60a91e0ed04
Mike Rivnak <rivnakm1@gmail.com>
2024-09-28 21:59:19 -0400

    Use gitversion to version on pr merge

--- scripts/SetVersion.ps1
+++
@@ -1,9 +0,0 @@
-$version = $args[0].replace("v", "")
-
-$files = @(
-    "./Cargo.toml"
-)
-
-foreach ($file in $files) {
-    ((Get-Content -path $file -Raw) -replace '0.0.0', $version) | Set-Content -Path $file
-}

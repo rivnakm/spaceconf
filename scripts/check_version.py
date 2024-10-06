7c8780ca636f73a53506d6841567f9b45d935486
Mike Rivnak <7389355+rivnakm@users.noreply.github.com>
2024-10-06 11:47:53 -0400

    chore(release): release from cargo.toml version (#34)

---
+++ scripts/check_version.py
@@ -0,0 +1,16 @@
+import semver
+import sys
+import toml
+
+tag_ver = sys.argv[1]
+tag_ver = tag_ver.replace("v", "")
+
+with open("Cargo.toml", "r") as f:
+    cargo_toml = toml.load(f)
+
+ver = cargo_toml["package"]["version"]
+
+if semver.compare(ver, tag_ver) == 1:
+    sys.exit(0)
+else:
+    sys.exit(1)
